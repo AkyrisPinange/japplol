81930779f66309f82f03585941926242ce817062
AkyrisPinange <48262372+AkyrisPinange@users.noreply.github.com>
2024-03-25 15:30:57 -0300

    Controllers

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,51 @@
+﻿using JAppInfos.Models;
+using JAppInfos.Models.login;
+using JAppInfos.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JAppInfos.Controllers
+{
+    
+    [ApiController]
+    public class LoginController : Controller
+    {
+        private readonly LoginService _loginService;
+        private readonly RegisterService _registerService;
+       public LoginController(LoginService loginService, RegisterService  registerService) 
+        {
+            _registerService = registerService;
+            _loginService = loginService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("login")]
+        public IActionResult Login([FromBody] UserLogin userLogin)
+        {
+            var token = _loginService.LoginUser(userLogin);
+
+            if (token != null)
+            {
+                return Ok(token);
+            }
+
+            return NotFound("User not found");
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("register")]
+        [Authorize]
+        public IActionResult Register([FromBody] User user)
+        {
+           _registerService.Register(user);
+
+                return Ok("Usuario Registrado com sucesso");
+
+        }
+
+     
+    }
+}
---
+++ Controllers/RiotController.cs
@@ -0,0 +1,52 @@
+﻿using JAppInfos.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JAppInfos.Controllers
+{
+    public class RiotController : Controller
+    {
+
+     
+        private readonly RegisterService _registerService;
+        private readonly RiotService _riotService;
+        public RiotController( RegisterService registerService, RiotService riotService)
+        {
+            _registerService = registerService;
+            _riotService = riotService;
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("consultSummoner")]
+        public async Task<IActionResult> ConsultSummonerAsync(string summonerName)
+        {
+            var summonerData = await _registerService.GetSummonerData(summonerName);
+            if (summonerData != null)
+            {
+                return Ok(summonerData);
+            }
+            else
+            {
+                return BadRequest("Deu errado");
+            }
+
+        }   
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("ChampionsSkins")]
+        public async Task<IActionResult> ChamptionAsync()
+        {
+            var champion = await _riotService.GetChamptionSkins();
+            if (champion != null)
+            {
+                return Ok(champion);
+            }
+            else
+            {
+                return BadRequest("Deu errado");
+            }
+
+        }
+    }
+}
