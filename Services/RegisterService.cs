b374fd98ce6d9fd89f21e871cb62e56c067cbb7c
AkyrisPinange <48262372+AkyrisPinange@users.noreply.github.com>
2024-03-25 15:31:38 -0300

    services

---
+++ Services/RegisterService.cs
@@ -0,0 +1,73 @@
+﻿using JAppInfos.Models.AppDbContext;
+using JAppInfos.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Text.Json;
+using JAppInfos.Models.handler;
+using JAppInfos.utils;
+
+
+
+
+namespace JAppInfos.Services
+{
+
+    public class RegisterService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<User> _userManager;
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly Utils _utils;
+
+
+
+        public RegisterService(ApplicationDbContext context, UserManager<User> userManager, IHttpClientFactory httpClientFactory, Utils utils)
+        {
+            _context = context;
+            _userManager = userManager;
+            _httpClientFactory = httpClientFactory;
+            _utils = utils;
+        }
+
+
+        public async void Register(User user)
+        {
+            var createUserResult = await _userManager.CreateAsync(user, user.PassWord); // Use UserManager
+            if (!createUserResult.Succeeded)
+            {
+                // Handle creation errors
+                throw new Exception("Failed to create user"); // Example error handling
+            }
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+        }
+
+            public async Task<object> GetSummonerData(string summonerName)
+            {
+                var client = _httpClientFactory.CreateClient("riotClient");
+                client.DefaultRequestHeaders.Add("X-Riot-Token", _utils.getConfig("baseUrl:Key"));
+
+                try
+                {
+                    var response = await client.GetAsync($"summoner/v4/summoners/by-name/{summonerName}");
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var content = await response.Content.ReadAsStringAsync();
+                        return JsonSerializer.Deserialize<SummonerData>(content);
+                    }
+                    else
+                    {
+                        var errorContent = await response.Content.ReadAsStringAsync();
+                        return JsonSerializer.Deserialize<ErrorResponse>(errorContent);
+
+                }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Error retrieving summoner data", ex); 
+                }
+            }
+        
+    }
+}
---
+++ Services/RiotService.cs
@@ -0,0 +1,91 @@
+﻿using JAppInfos.Models.champion;
+using System.Text.Json;
+
+namespace JAppInfos.Services
+{
+    public class RiotService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public RiotService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<Dictionary<string, ChampionList>> GetChampions()
+        {
+            try
+            {
+                HttpClient client = _httpClientFactory.CreateClient("ddragon");
+                HttpResponseMessage response = await client.GetAsync("champion.json");
+
+                 response.EnsureSuccessStatusCode();
+
+                string jsonString = await response.Content.ReadAsStringAsync();
+                JsonElement championsJson = JsonDocument.Parse(jsonString).RootElement.GetProperty("data"); 
+                if (CheckJsonElement(championsJson))
+                {
+                   
+                    throw new Exception("Failed to parse champions data"); 
+                }
+                return JsonSerializer.Deserialize<Dictionary<string, ChampionList>>(championsJson);
+            }
+            catch (HttpRequestException ex)
+            {
+                throw new HttpRequestException($"Failed to fetch champions: {ex.Message}");
+            }
+        }
+
+        public async Task<Dictionary<string, Champion>> GetChampionByName(string name) 
+        {
+            HttpClient client = _httpClientFactory.CreateClient("ddragon");
+            HttpResponseMessage response = await client.GetAsync($"champion/{name}.json");
+
+            response.EnsureSuccessStatusCode();
+
+            string jsonString = await response.Content.ReadAsStringAsync();
+            JsonElement championjson = JsonDocument.Parse(jsonString).RootElement.GetProperty("data");
+            if (CheckJsonElement(championjson)) 
+            {
+                throw new Exception("Failed to parse champion data");
+            }
+            return JsonSerializer.Deserialize<Dictionary<string, Champion>>(championjson);
+        }
+
+        private bool CheckJsonElement(JsonElement jsonElement) {
+            return jsonElement.ValueKind == JsonValueKind.Null 
+                || jsonElement.ValueKind == JsonValueKind.Undefined 
+                || jsonElement.Equals("");
+        }
+
+        public async Task<Dictionary<string, List<Skin>>> GetChamptionSkins()
+        {
+            Dictionary<string, ChampionList> champions = await GetChampions();
+            Dictionary<string, List<Skin>> championSkins = new Dictionary<string, List<Skin>>();
+
+
+            foreach (var championEntry in champions)
+            {
+                string championName = championEntry.Key;
+               // Champion champion = championEntry.Value;
+
+                // Retrieve skins for the current champion
+                List<Skin> skins = await GetChampionSkinsByName(championName);
+
+                // Add champion skins to the dictionary
+                championSkins.Add(championName, skins);
+            }
+
+            return championSkins;
+
+        }
+        private async Task<List<Skin>> GetChampionSkinsByName(string championName)
+        {
+            Dictionary<string, Champion> champion = await GetChampionByName(championName);
+            List<Skin> skins = champion.Values.First().Skins;
+
+            return skins;
+
+        }
+    }
+}
